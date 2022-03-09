fd0e4b7bfdfd793d7887ed587487cf796c61fc42
yeenbean <7747985+yeenbean@users.noreply.github.com>
2022-03-09 06:59:21 -0700

    Created script to clean up XML file after export
    tidy.ps1 will clean up the XML file after exporting from Ketarin.  This will keep the change history from getting messy, as well as importing recent file locations that do not exist.

---
+++ tidy.ps1
@@ -0,0 +1,12 @@
+$xml = [xml](Get-Content "All software.xml")
+
+$nodes = $xml.SelectNodes("//PreviousLocation")
+foreach($node in $nodes){$node.ParentNode.RemoveChild($node)}
+
+$nodes = $xml.SelectNodes("//LastUpdated")
+foreach($node in $nodes){$node.ParentNode.RemoveChild($node)}
+
+$nodes = $xml.SelectNodes("//LastFileDate")
+foreach($node in $nodes){$node.ParentNode.RemoveChild($node)}
+
+$xml.Save("All software.xml")
